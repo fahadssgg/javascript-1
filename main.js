7a0c5b4c5789ef2bc994a3910d239094a1e69bee
fahadssgg <fahadguzai@gmail.com>
2023-08-07 17:33:51 +0300

    if-lap

--- main.js
+++ main.js
@@ -1,3 +1,40 @@
+console.log("//////////////even & odd ///////////////////");
+
+let n = 24;
+if (n % 2 == 0) {
+  console.log("nuber is even");
+} else {
+  console.log("nuber is odd");
+}
+
+console.log("//////////////grade ///////////////////");
+
+let Grade = 55;
+if (Grade >= 101) {
+  console.log("what is this???");
+} else if (Grade >= 90) {
+  console.log("A");
+} else if (Grade >= 80) {
+  console.log("B");
+} else if (Grade >= 70) {
+  console.log("C");
+} else if (Grade >= 60) {
+  console.log("D");
+} else if (Grade <= 59) {
+  console.log("F");
+}
+
+console.log("//////////////Fizz Buzz ///////////////////");
+
+let nFB = 15;
+if (nFB % 5 == 0 && nFB % 3 == 0) {
+  console.log("Fizz-Buzz");
+} else if (nFB % 5 == 0) {
+  console.log("Buzz");
+} else if (nFB % 3 == 0) {
+  console.log("fizz");
+}
+
 console.log("//////////////square ///////////////////");
 for (let i = 1; i <= 10; i++) {
   console.log(Math.pow(i, 2));
